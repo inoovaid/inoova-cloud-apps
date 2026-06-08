fa48ac1be2ca8c182406964a3d7042bfa644b6e6
inoovaid <inoovaid@inoovaid.com.br>
2026-06-08 06:07:21 +0000

    feat: add langgraph oss agent

--- AIOps/langgraph-app/graph.py
+++ AIOps/langgraph-app/graph.py
@@ -1,18 +1,43 @@
-from fastapi import FastAPI
-from langchain_ollama import ChatOllama
+from typing import TypedDict
 
-app = FastAPI()
+from langgraph.graph import StateGraph, END
+from langchain_ollama import ChatOllama
 
 llm = ChatOllama(
     model="gemma3:4b",
     base_url="http://ollama-agents.ai-system.svc.cluster.local:11434"
 )
 
-@app.get("/")
-def root():
-    return {"status": "ok"}
+class AgentState(TypedDict):
+    question: str
+    answer: str
 
-@app.get("/chat")
-def chat(q: str):
-    response = llm.invoke(q)
-    return {"response": response.content}
+def chat_node(state):
+
+    response = llm.invoke(state["question"])
+
+    return {
+        "answer": response.content
+    }
+
+graph = StateGraph(AgentState)
+
+graph.add_node("chat", chat_node)
+
+graph.set_entry_point("chat")
+
+graph.add_edge("chat", END)
+
+agent = graph.compile()
+
+while True:
+
+    question = input("Pergunta: ")
+
+    result = agent.invoke(
+        {
+            "question": question
+        }
+    )
+
+    print(result["answer"])
